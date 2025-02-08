b22dcb7424581be0637a75d48f9dd8af5431dc31
Ricardo Valero de la Rosa <55701657+ricardo-valero@users.noreply.github.com>
2025-02-08 11:04:26 -0600

    remove flake utils from template

--- nix/templates/simple/flake.nix
+++ nix/templates/simple/flake.nix
@@ -3,36 +3,34 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
     roc.url = "github:roc-lang/roc";
   };
 
-  outputs = { self, nixpkgs, flake-utils, roc, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-
-        # see "packages =" in https://github.com/roc-lang/roc/blob/main/flake.nix
-        rocPkgs = roc.packages.${system};
-
-        rocFull = rocPkgs.full;
-
-      in
-      {
-        formatter = pkgs.nixpkgs-fmt;
-
-        devShells = {
+  outputs = { nixpkgs, roc }:
+    let
+      systems = nixpkgs.lib.platforms.all;
+      forEachSystem = nixpkgs.lib.genAttrs systems;
+    in {
+      formatter = forEachSystem (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in pkgs.nixpkgs-fmt);
+      devShells = forEachSystem (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          # see "packages =" in https://github.com/roc-lang/roc/blob/main/flake.nix
+          rocPkgs = roc.packages.${system};
+          rocFull = rocPkgs.full;
+        in {
           default = pkgs.mkShell {
-            buildInputs = with pkgs;
-              [
-                rocFull # includes CLI
-              ];
+            buildInputs = [
+              rocFull # includes CLI
+            ];
 
             # For vscode plugin https://github.com/ivan-demchenko/roc-vscode-unofficial
             shellHook = ''
               export ROC_LANGUAGE_SERVER_PATH=${rocFull}/bin/roc_language_server
             '';
           };
-        };
-      });
+        });
+    };
 }
