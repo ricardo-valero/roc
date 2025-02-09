55644d7e75f8b663aa0cf27f48f9f27a9f1a0fd6
Ricardo Valero de la Rosa <55701657+ricardo-valero@users.noreply.github.com>
2025-02-09 01:07:35 -0600

    simplify compile deps

--- nix/compile-deps.nix
+++ nix/compile-deps.nix
@@ -2,24 +2,12 @@
 let
   zigPkg = pkgs.zig;
   llvmPkgs = pkgs.llvm_18;
-  llvmVersion = builtins.splitVersion llvmPkgs.release_version;
-  llvmMajorMinorStr = builtins.elemAt llvmVersion 0 + builtins.elemAt llvmVersion 1;
   # nix does not store libs in /usr/lib or /lib
-  glibcPath =
-    if pkgs.stdenv.isLinux then "${pkgs.glibc.out}/lib" else "";
+  glibcPath = if pkgs.stdenv.isLinux then "${pkgs.glibc.out}/lib" else "";
   libGccSPath =
     if pkgs.stdenv.isLinux then "${pkgs.stdenv.cc.cc.lib}/lib" else "";
-in
-{
-  inherit zigPkg llvmPkgs llvmVersion llvmMajorMinorStr glibcPath libGccSPath;
-
-  darwinInputs = with pkgs;
-    lib.optionals stdenv.isDarwin
-      (with pkgs.darwin.apple_sdk.frameworks; [
-        AppKit
-        CoreFoundation
-        CoreServices
-        Foundation
-        Security
-      ]);
-}
+  darwinInputs = pkgs.lib.optionals pkgs.stdenv.isDarwin (builtins.attrValues {
+    inherit (pkgs.darwin.apple_sdk.frameworks)
+      AppKit CoreFoundation CoreServices Foundation Security;
+  });
+in { inherit zigPkg llvmPkgs glibcPath libGccSPath darwinInputs; }
